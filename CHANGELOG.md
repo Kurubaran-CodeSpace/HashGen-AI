b6a3accf63adfd53c3abddb9904620f98f3e9dab
Kurubaran Anandhan <akurubaran@myyahoo.com>
2024-06-07 15:18:44 +0530

    Release v1.0.2 - Enhance Example App and Add Comments
    This release includes the following updates:
    
    * Added detailed production-level comments to the example app in `main.dart`.
    * Enhanced the readability and maintainability of the example code.
    * Provided a demonstration of how to use the HashGenAI package in a Flutter app.
    
    The example app now includes comprehensive comments explaining each part of the code, making it easier for developers to understand and use the HashGenAI package.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,17 @@
+## 1.0.2
+
+* Added detailed production-level comments to the example app in `main.dart`.
+* Enhanced the readability and maintainability of the example code.
+* Provided a comprehensive demonstration of how to use the HashGenAI package in a Flutter app.
+
 ## 1.0.1
 
 * Minor Bug Fixes.
+
+## 1.0.0
+
+* Initial release of `hashgen_ai`
+* Generate hashtags based on the content provided
+* Specify the number of hashtags to generate
+* Easy integration with OpenAI API
+* Supports multiple platforms: Android, iOS, web, and macOS
--- test/hashgen_ai_test.dart
+++ test/hashgen_ai_test.dart
@@ -6,8 +6,11 @@ import 'dart:convert';
 
 void main() {
   group('HashGenAI Tests', () {
+    // Test case for generating hashtags using the HashGenAI class
     test('Generate Hashtags Test', () async {
+      // Mock HTTP client to simulate the OpenAI API response
       final mockClient = MockClient((request) async {
+        // Simulated JSON response from OpenAI API
         final mapJson = {
           'choices': [
             {
@@ -15,13 +18,20 @@ void main() {
             }
           ]
         };
+        // Return the mock response with a status code of 200 (OK)
         return http.Response(json.encode(mapJson), 200);
       });
 
+      // Initialize the HashGenAI instance with the mock client
       final hashGenAI = HashGenAI('YOUR_OPENAI_API_KEY', client: mockClient);
+
+      // Call the generateHashtags method with test content and the number of hashtags
       final hashtags = await hashGenAI.generateHashtags('Flutter is great', 5);
 
+      // Verify that the number of generated hashtags is correct
       expect(hashtags.length, 5);
+
+      // Verify that the generated hashtags contain the expected values
       expect(hashtags, containsAll(['#flutter', '#dart', '#programming', '#opensource', '#developer']));
     });
   });
